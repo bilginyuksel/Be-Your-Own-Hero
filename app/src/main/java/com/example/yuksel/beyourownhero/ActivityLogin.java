e70add914f4624a08441f185b5e88cb6a0a22764
bilginyuksel <bilginyuksel@users.noreply.github.com>
2018-12-02 22:58:31 +0300

    .

--- app/src/main/java/com/example/yuksel/beyourownhero/ActivityLogin.java
+++ app/src/main/java/com/example/yuksel/beyourownhero/ActivityLogin.java
@@ -1,6 +1,10 @@
 package com.example.yuksel.beyourownhero;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,77 +13,111 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 
 public class ActivityLogin extends AppCompatActivity {
 
+    static ArrayList<Word> arrWords = new ArrayList<>();
+    TextView txtCat,txtWord;
+
+    public void __init__(){
+        txtCat = (TextView)findViewById(R.id.txtCategory);
+        txtWord = (TextView)findViewById(R.id.txtMotivationWord);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //__init__();
+
         FirebaseDatabase db = new FirebaseDatabase();
+       /* Word d = new Word(db.uuid(),"Dont Sleep Wake up and Do something","Motivation");
+        db.addToHost("Word",d,db.uuid());
+        Word d1 = new Word(db.uuid(),"How many punches you got is not matter ","Sport");
+        db.addToHost("Word",d1,db.uuid());
+        Word d2 = new Word(db.uuid(),"Stupid coward im going to fuck you","Business");
+        db.addToHost("Word",d2,db.uuid());
+
+*/
         db.getWords();
-        TextView txtCategory = (TextView)findViewById(R.id.txtCategory);
-        TextView txtMotivationWord = (TextView)findViewById(R.id.txtMotivationWord);
-        try {
-            txtCategory.setText("" + FirebaseDatabase.arrWords.get(0).getCategory());
-            txtMotivationWord.setText("" + FirebaseDatabase.arrWords.get(0).getMotivationWord());
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        ImageButton wordOfDay = (ImageButton)findViewById(R.id.imgButtonWordofDay);
-        ArrayList<User> arrayUser = new ArrayList<>();
+
+
+
+
+        ImageButton btnWordOfDay = (ImageButton)findViewById(R.id.imgButtonWordofDay);
+        ImageButton btnalarm = (ImageButton)findViewById(R.id.btnAlarm);
+        ImageButton btnProfile = (ImageButton)findViewById(R.id.btnProfile);
+
+
+
+
         final ArrayList<String> arrSt = new ArrayList<>();
         arrSt.add("toplantı");
-
-
-        ArrayAdapter arrAdapt = new ArrayAdapter(this,android.R.layout.simple_list_item_multiple_choice,arrSt);
-
-
-
+        ArrayAdapter arrAdapt = new ArrayAdapter(this,android.R.layout.select_dialog_multichoice,arrSt);
         final ListView lstTry = (ListView)findViewById(R.id.lstview);
-
         lstTry.setAdapter(arrAdapt);
-        wordOfDay.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+
+
+        btnWordOfDay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Call alert
                 showWordOfTheDay();
 
             }
         });
-
-
-
-
-//        lstTry.setAdapter(arrAdapt);
-     //
-         lstTry.setChoiceMode(lstTry.CHOICE_MODE_MULTIPLE);
-
-      /*  ListView lstView = (ListView)findViewById(R.id.listview1);
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayUser);
-        User user = new User("yuksel","bilgin","bilginyuksel","123456");
-        User user1 = new User("hasancan","kazan","hck","221133");
-
-        arrayUser.add(user);
-        arrayUser.add(user1);
-        lstView.setAdapter(adapter);
-
-*/
-
+        btnalarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlarm();
+            }
+        });
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showProfile();
+            }
+        });
     }
+    //ALERT FUNCTIONS
     public void showWordOfTheDay(){
-
         Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.alert_wordofday);
         dialog.setCancelable(true);
+        txtCat = dialog.findViewById(R.id.txtCategory);
+        txtWord = dialog.findViewById(R.id.txtMotivationWord);
+        txtCat.setText(""+arrWords.get(0).getCategory());
+        txtWord.setText(""+arrWords.get(0).getMotivationWord());
 
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        for(Word v : arrWords){
+            //Need to declare textview ...
+            System.out.println("Category: "+ v.getCategory() + "\nWord: "+v.getMotivationWord());
+        }
         dialog.show();
 
+    }
+    public void showAlarm(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.alarm_manager);
+        dialog.setCancelable(true);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
+        dialog.show();
+    }
+    public void showProfile(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.heroesprofile);
+        dialog.setCancelable(true);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
+        dialog.show();
     }
 }
--- app/src/main/java/com/example/yuksel/beyourownhero/FirebaseDatabase.java
+++ app/src/main/java/com/example/yuksel/beyourownhero/FirebaseDatabase.java
@@ -7,17 +7,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FirebaseDatabase extends Firebase {
+
     static Word words;
-    static ArrayList<Word> arrWords = new ArrayList<>();
-
-
     @Override
     public void addToHost(String refName,Object o,String uuid) {
         pickReference(refName).child(uuid).setValue(o);
     }
-
 
     @Override
     public void getWords() {
@@ -26,13 +24,16 @@ public class FirebaseDatabase extends Firebase {
        pickReference("Word").addValueEventListener(new ValueEventListener() {
            @Override
            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-               words = dataSnapshot.getChildren().iterator().next().getValue(Word.class);
-               arrWords.add(dataSnapshot.getChildren().iterator().next().getValue(Word.class));
-               //it could expand with arrayList or use query orderbychild..
-               //use alertdialog for load array.... or something else
-               //you need the word of day if you cant load it dont touch it
-               System.out.println(words);
+               //words = dataSnapshot.getChildren().iterator().next().getValue(Word.class);
+
+               //Fill array with words.
+               int index=0;
+               for(DataSnapshot sp: dataSnapshot.getChildren()){
+                   HashMap<String,String> map = (HashMap<String, String>)sp.getValue();
+                   ActivityLogin.arrWords.add(index++,new Word(map.get("id"),map.get("motivationWord"),map.get("category")));
+               }
            }
+
 
            @Override
            public void onCancelled(@NonNull DatabaseError databaseError) {
